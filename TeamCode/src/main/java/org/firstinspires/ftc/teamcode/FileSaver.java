14714e2cc91cd6f131772cfdfbf80fdf32862dce
coleh2 <coleh@coleh.net>
2019-10-02 07:02:32 -0400

    Use reflection-based Context getting for FileSaver

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileSaver.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.app.Application;
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FileSaver {
+    private Context context;
+    public String fileName;
+
+    public FileSaver(String _fileName) {
+        try {
+            this.context = ((Application) Class.forName("android.app.ActivityThread").getMethod("currentApplication").invoke(null, (Object[]) null)).getApplicationContext();
+        } catch(Exception e) {}
+        this.fileName = _fileName;
+    }
+    public FileSaver() {}
+
+    public ArrayList<String> readLines() {
+
+            String thisLine;
+            BufferedReader textFile;
+            ArrayList<String> keyframes = new ArrayList<String>();
+
+            try {
+                textFile = new BufferedReader(new InputStreamReader(new FileInputStream(context.getFilesDir().getPath() + "/" + fileName)));
+                BufferedReader br = new BufferedReader(textFile);
+
+                while ((thisLine = br.readLine()) != null) {
+                    keyframes.add(thisLine);
+                }
+            } catch (Exception e) {
+                //e.printStackTrace();
+            }
+            return keyframes;
+
+    }
+
+    public void appendLine(String line) {
+        try {
+            BufferedWriter output = new BufferedWriter(new FileWriter(context.getFilesDir().getPath() + "/" + fileName, true));
+            output.newLine();
+            output.write(line);
+        } catch (Exception e) {
+            //e.printStackTrace
+        }
+    }
+
+
+}

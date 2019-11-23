03e5cec9ab49e46b70d11af00ebc331380ca8721
Duelephant <pmb221@students.needham.k12.ma.us>
2019-11-22 19:59:14 -0500

    current code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBL.java
@@ -25,26 +25,26 @@ public class AutoBL extends StepAuto {
     public void loop() {
        switch(currentStep){
         case START:
-            driver.driveOmni(0f, 1f, 0f);
+            driver.driveOmni(0f, -0.5f, 0f);
             nextStep(1000);
         break;
-        case MOVE1:
-
-            //Drop bar t
-            // driver.driveOmni(0f, 0f, 0f);
-            nextStep(1000);
-        case MOVE2:
-            driver.driveOmni(-1f, 0f, 0f);
-            nextStep(1000);
-        break;
-        case MOVE3:
-            driver.driveOmni(0f, -1f, 0f);
-            nextStep(500);
-        break;
-        case MOVE4:
-            driver.driveOmni(1f, 0f, 0f);
-            nextStep(1000);
-        break;
+//        case MOVE1:
+////
+////            //Drop bar t
+////            // driver.driveOmni(0f, 0f, 0f);
+////            nextStep(1000);
+////        case MOVE2:
+////            driver.driveOmni(-1f, 0f, 0f);
+////            nextStep(1000);
+////        break;
+////        case MOVE3:
+////            driver.driveOmni(0f, -1f, 0f);
+////            nextStep(500);
+////        break;
+////        case MOVE4:
+////            driver.driveOmni(1f, 0f, 0f);
+////            nextStep(1000);
+////        break;
         default:
             driver.driveOmni(0f, 0f, 0f);
 

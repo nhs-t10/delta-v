faf34c2f86afce847153b08fffbb2b4c2051167e
Duelephant <pmb221@students.needham.k12.ma.us>
2020-01-15 00:05:49 -0500

    THIS CODE HAS ERRORS in Telemetry manager

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryManager.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
@@ -12,14 +13,25 @@ import org.firstinspires.ftc.robotcore.internal.opengl.models.Geometry;
 import org.firstinspires.ftc.teamcode.*;
 import org.firstinspires.ftc.teamcode.data.*;
 import org.firstinspires.ftc.teamcode.auxillary.*;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Handle all movement of the chassis.
- */
+ * Make Telemetry pretty
+ */ 
 public class TelemetryManager extends FeatureManager {
 
-    public Telemetry telemetry;
+    public OpMode opMode;
+
+    public int currentTab;
+
+    public ArrayList<String> keys;
+    public ArrayList<String> values;
+
+    public HashMap<String, boolean> otflags;
+
+    private RobotState lastState;
     /**
      * Create a TelemetryManager with four motors.
      * @param fl Front Left motor
@@ -27,19 +39,48 @@ public class TelemetryManager extends FeatureManager {
      * @param br Back Right motor
      * @param bl Back Left motor
      */
-
-    private static float speed = 1.0f;
-
-    public TelemetryManager(Telemetry _telemetry) {
+    public TelemetryManager(OpMode opmode) {
         
-        this.telemetry = _telemetry;
-    }
-    public TelemetryManager(){ }
+        this.opMode = _opmode;
+        this.currentTab = 0;
 
-    /**
-     * 
-     */
-    public void driveRaw(float fl, float fr, float br, float bl) {
+        keys = new ArrayList<String>();
+        values = new ArrayList<String>();
+        otflags = new HashMap<String, boolean>();
     }
+
+
+    public void update(RobotState state) {
+        this.lastState = state;
+        this.update();
+    }
+
+    public void oneTimeFlag(String flag) {
+        otflags.put(flag, true);
+    }
+
+    public void update() {
+        String[] arrKeys = keys.toArray();
+
+        if(currentTab == 1) {
+            //OpMode-inserted values
+            for(int i = 0; i < arrKeys.length; i++) {
+                String key = arrKeys[i];
+                String val = values.get(i);
+
+                opMode.telemetry.addData(key, val);
+            }
+            //one-time flags
+            for()
+        } else if(currentTab == 0) {
+            telemetry
+        } 
+
+        opMode.telemetry.update();
+    }
+
+
+
+    public TelemetryManager(){ }
 
 }
